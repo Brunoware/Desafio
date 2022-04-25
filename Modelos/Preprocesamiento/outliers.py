583b5cec57125e3e10940fa25729ffb8e9e41744
Bruno <bruno.miranda@pucp.edu.pe>
2022-04-25 12:59:44 -0500

    Correcion de errores 1

--- Modelos/Preprocesamiento/outliers.py
+++
@@ -1,9 +0,0 @@
-# Se impora la libreria para plotear
-import matplotlib.pyplot as plt
-
-
-def boxplot (df,list_columns):
-    for columna in list_columns:
-        df.boxplot(column = columna)
-        plt.title(columna)
-        plt.show()
